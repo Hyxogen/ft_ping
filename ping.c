f06c1c1d59e558dee68269bde29c406d3b5ddc0a
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-02-16 15:00:52 +0100

    simple ping

--- ping.c
+++ ping.c
@@ -1,92 +1,141 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
-#include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <netinet/ip_icmp.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
-struct icmp_header {
-  uint8_t type;
-  uint8_t code;
-  uint16_t check;
-  uint32_t rem;
+static const char *program_name = NULL;
+
+struct icmp_echo {
+	struct icmphdr header;
+	unsigned char data[];
 };
 
-void rwrite(int fd, const void *data, size_t n, struct sockaddr *addr, socklen_t len)
+struct ping_ctx {
+	int sockfd;
+	struct sockaddr_in addr;
+	struct icmp_echo base;
+	size_t baselen;
+};
+
+static void error(const char *s)
 {
-  printf("trying to write %zu bytes to fd %i, socklen: %zu\n", n, fd, len);
-  int rc = sendto(fd, data, n, 0, addr, len);
-  //int rc = write(fd, data, n);
-  if (rc < 0) {
-    perror("write");
-    abort();
-  }
-  printf("wrote something\n");
+	const char *err = strerror(errno);
+	if (s)
+		fprintf(stderr, "%s: %s\n", s, err);
+	else
+		fprintf(stderr, "%s: %s\n", program_name, err);
+}
+
+static int do_ping(const struct ping_ctx *ctx)
+{
+	//printf("%i, %p, %zu, %p, %u\n", ctx->sockfd, &ctx->base, ctx->baselen, addr, len);
+	ssize_t nsent = sendto(ctx->sockfd, &ctx->base, ctx->baselen, 0,
+			       (const struct sockaddr *)&ctx->addr,
+			       sizeof(ctx->addr));
+	if (nsent < 0 || nsent < ctx->baselen) {
+		error("sendto");
+		return 1;
+	}
+	printf("wrote %li bytes\n", nsent);
+
+	char buf[1024];
+	struct iovec iov;
+	iov.iov_base = buf;
+	iov.iov_len = sizeof(buf);
+
+	char name[128];
+	char cbuf[512];
+	struct msghdr msg;
+
+	msg.msg_name = name;
+	msg.msg_namelen = sizeof(name);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = cbuf;
+	msg.msg_controllen = sizeof(cbuf);
+	msg.msg_flags = 0;
+
+	ssize_t nread = recvmsg(ctx->sockfd, &msg, 0);
+	if (nread < 0) {
+		error("recvmsg");
+		return 1;
+	}
+
+	fprintf(stdout, "%u bytes\n", (unsigned) nread);
+	return 0;
+}
+
+// TODO check for overflow
+static uint16_t checksum(const void *src, size_t len)
+{
+	const uint8_t *s = src;
+
+	uint32_t sum = 0;
+	size_t i = 0;
+	for (; i + 1 < len; i += 2) {
+		uint16_t word = ((uint16_t) s[i] << 8) + s[i + 1];
+		sum += word;
+	}
+	if (i < len) {
+		assert(0);
+		sum += ((uint32_t) s[i] << 8);
+	}
+
+	//printf("now: %x\n", sum);
+	while (sum & ~0xFFFF) {
+		sum = (sum & 0xFFFF) + (sum >> 16);
+	}
+	return (uint16_t) ~sum;
 }
 
 int main(int argc, char **argv)
 {
-  struct addrinfo hints, *res;
+	program_name = argv[0];
+	if (argc < 2) {
+		fprintf(stderr, "%s: destination address required\n", argv[0]);
+		return EXIT_SUCCESS;
+	}
 
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  //hints.ai_socktype = SOCK_DGRAM;
-  hints.ai_protocol = IPPROTO_ICMP;
-  //hints.ai_socktype = SOCK_RAW;
-  //hints.ai_protocol = getprotobyname("icmp")->p_proto;
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof(hints));
+	//hints.ai_family = AF_INET;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_protocol = IPPROTO_ICMP;
 
-  int rc = getaddrinfo("127.0.0.1", NULL, &hints, &res);
-  if (rc) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
-    abort();
-  }
-  
-  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+	int rc = getaddrinfo(argv[1], NULL, &hints, &res);
+	if (rc < 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
+		return EXIT_FAILURE;
+	}
 
-  printf("fd: %i\n", sockfd);
-  struct icmphdr hdr;
-  memset(&hdr, 0, sizeof hdr);
-  hdr.type = ICMP_ECHO;
-  hdr.un.echo.id = 1234;
-  hdr.checksum = htons(~((uint16_t) 8));
-  rwrite(sockfd, &hdr, sizeof hdr, res->ai_addr, res->ai_addrlen);
+	struct ping_ctx ctx;
+	ctx.sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+	if (ctx.sockfd < 0) {
+		error("socket");
+		return EXIT_FAILURE;
+	}
 
-  /*char buffer[1024];
-  memset(buffer, 0, sizeof(buffer));
+	ctx.addr = *(struct sockaddr_in*) res->ai_addr;
+	ctx.addr.sin_family = AF_INET;
+	ctx.addr.sin_port = ((struct sockaddr_in*) res->ai_addr)->sin_port;
+	ctx.addr.sin_addr = ((struct sockaddr_in*) res->ai_addr)->sin_addr;
 
-  ssize_t nread = read(sockfd, &buffer, sizeof buffer);
-  if (nread < 0) {
-          perror("read");
-          abort();
-  }
-  printf("read %i bytes\n", nread);
+	ctx.base.header.type = ICMP_ECHO;
+	ctx.base.header.code = 0;
+	ctx.base.header.checksum = 0;
+	ctx.base.header.un.echo.id = 1234;
+	ctx.base.header.un.echo.sequence = 0;
+	ctx.baselen = sizeof(ctx.base);
 
-  for (ssize_t i = 0; i < nread; ++i) {
-          printf("%02hhx ", (unsigned char)buffer[i]);
-  }
-  printf("\n");*/
+	ctx.base.header.checksum = checksum(&ctx, ctx.baselen);
 
-  char buffer[1024];
-  struct iovec data;
-  struct msghdr msg;
-  memset(&msg, 0, sizeof msg);
-  data.iov_base = buffer;
-  data.iov_len = sizeof buffer;
-  msg.msg_iov = &data;
-  msg.msg_iovlen = 1;
-
-  ssize_t nread = recvmsg(sockfd, &msg, 0);
-  if (nread < 0) {
-          perror("read");
-          abort();
-  }
-  printf("read %i bytes\n", nread);
-
-  for (ssize_t i = 0; i < nread; ++i) {
-          printf("%02hhx ", (unsigned char)buffer[i]);
-  }
-  printf("\n");
+	if (do_ping(&ctx))
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
 }
---
+++ ping_old.c
@@ -0,0 +1,108 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/ip_icmp.h>
+
+struct icmp_header {
+	uint8_t type;
+	uint8_t code;
+	uint16_t check;
+	uint32_t rem;
+};
+
+void rwrite(int fd, const void *data, size_t n, struct sockaddr *addr,
+	    socklen_t len)
+{
+	printf("trying to write %zu bytes to fd %i, socklen: %zu\n", n, fd,
+	       len);
+	int rc = sendto(fd, data, n, 0, addr, len);
+	//int rc = write(fd, data, n);
+	if (rc < 0) {
+		perror("write");
+		abort();
+	}
+	printf("wrote something\n");
+}
+
+int main(int argc, char **argv)
+{
+	struct addrinfo hints, *res;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	//hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_ICMP;
+	//hints.ai_socktype = SOCK_RAW;
+	//hints.ai_protocol = getprotobyname("icmp")->p_proto;
+
+	int rc = getaddrinfo("127.0.0.1", NULL, &hints, &res);
+	if (rc) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
+		abort();
+	}
+
+	int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+	int yes = 1;
+	assert(!setsockopt(sockfd, SOL_IP, IP_RECVERR, &yes, sizeof(yes)));
+	assert(!setsockopt(sockfd, SOL_IP, IP_RECVTTL, &yes, sizeof(yes)));
+	assert(!setsockopt(sockfd, SOL_IP, IP_RETOPTS, &yes, sizeof(yes)));
+
+	printf("fd: %i\n", sockfd);
+	struct icmphdr hdr;
+	memset(&hdr, 0, sizeof hdr);
+	hdr.type = ICMP_ECHO;
+	hdr.un.echo.id = 1234;
+	hdr.checksum = htons(~((uint16_t)8));
+	rwrite(sockfd, &hdr, sizeof hdr, res->ai_addr, res->ai_addrlen);
+
+	/*char buffer[1024];
+  memset(buffer, 0, sizeof(buffer));
+
+  ssize_t nread = read(sockfd, &buffer, sizeof buffer);
+  if (nread < 0) {
+          perror("read");
+          abort();
+  }
+  printf("read %i bytes\n", nread);
+
+  for (ssize_t i = 0; i < nread; ++i) {
+          printf("%02hhx ", (unsigned char)buffer[i]);
+  }
+  printf("\n");*/
+
+	char buffer[1024];
+	struct iovec data;
+	struct msghdr msg;
+	memset(&msg, 0, sizeof msg);
+	data.iov_base = buffer;
+	data.iov_len = sizeof buffer;
+	msg.msg_iov = &data;
+	msg.msg_iovlen = 1;
+
+	char cbuf[512];
+	msg.msg_control = cbuf;
+	msg.msg_controllen = sizeof(cbuf);
+
+	char name[512];
+	msg.msg_name = name;
+	msg.msg_namelen = sizeof(name);
+
+	ssize_t nread = recvmsg(sockfd, &msg, 0);
+	if (nread < 0) {
+		perror("read");
+		abort();
+	}
+	printf("read %i bytes\n", nread);
+
+	for (ssize_t i = 0; i < nread; ++i) {
+		printf("%02hhx ", (unsigned char)buffer[i]);
+	}
+	printf("\n");
+
+	printf("control_len: %zu\n", msg.msg_controllen);
+}
