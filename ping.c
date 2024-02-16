8f63f109d61a84a20e74d5cede62481c3ef058cc
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-02-16 08:23:36 +0100

    send and receive ping

--- ping.c
+++ ping.c
@@ -17,8 +17,9 @@ struct icmp_header {
 
 void rwrite(int fd, const void *data, size_t n, struct sockaddr *addr, socklen_t len)
 {
-  printf("trying to write to %i\n", fd);
+  printf("trying to write %zu bytes to fd %i, socklen: %zu\n", n, fd, len);
   int rc = sendto(fd, data, n, 0, addr, len);
+  //int rc = write(fd, data, n);
   if (rc < 0) {
     perror("write");
     abort();
@@ -32,9 +33,10 @@ int main(int argc, char **argv)
 
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_DGRAM;
+  //hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_protocol = IPPROTO_ICMP;
   //hints.ai_socktype = SOCK_RAW;
-  //hints.ai_protocol = IPPROTO_ICMP;
+  //hints.ai_protocol = getprotobyname("icmp")->p_proto;
 
   int rc = getaddrinfo("127.0.0.1", NULL, &hints, &res);
   if (rc) {
@@ -42,59 +44,49 @@ int main(int argc, char **argv)
     abort();
   }
   
-  //int sockfd = -1;
-  int sockfd = socket(AF_INET, SOCK_DGRAM, getprotobyname("icmp")->p_proto);
-  if (sockfd < 0) {
-    perror("socket");
-    abort();
-  }
-
-  /*while (res) {
-    printf("trying\n");
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sockfd < 0) {
-      res = res->ai_next;
-      continue;
-    }
-    rc = connect(sockfd, res->ai_addr, res->ai_addrlen);
-    if (rc == -1) {
-      close(sockfd);
-      res = res->ai_next;
-      continue;
-    }
-    break;
-  }
-  if (!res) {
-    perror("failed to connect");
-    abort();
-  }*/
+  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 
   printf("fd: %i\n", sockfd);
   struct icmphdr hdr;
   memset(&hdr, 0, sizeof hdr);
   hdr.type = ICMP_ECHO;
   hdr.un.echo.id = 1234;
-  rwrite(sockfd, &hdr, sizeof hdr, res->ai_addr, res->ai_protocol);
+  hdr.checksum = htons(~((uint16_t) 8));
+  rwrite(sockfd, &hdr, sizeof hdr, res->ai_addr, res->ai_addrlen);
 
-  /*abort();
-  uint8_t type = 8;
-  rwrite(sockfd, &type, sizeof(type), res->ai_addr, res->ai_protocol);
-  uint8_t code = 0;
-  rwrite(sockfd, &code, sizeof(code), res->ai_addr, res->ai_protocol);
-  uint16_t checksum = htons(~((uint16_t) 8));
-  rwrite(sockfd, &checksum, sizeof(checksum), res->ai_addr, res->ai_protocol);
-  uint32_t ident_seq = 0;
-  rwrite(sockfd, &ident_seq, sizeof(checksum), res->ai_addr, res->ai_protocol);
-  fsync(sockfd);
-  printf("written echo\n");*/
+  /*char buffer[1024];
+  memset(buffer, 0, sizeof(buffer));
 
-  struct icmp_header header;
-  memset(&header, 0, sizeof header);
-  ssize_t nread = read(sockfd, &header, sizeof header);
+  ssize_t nread = read(sockfd, &buffer, sizeof buffer);
   if (nread < 0) {
-    perror("read");
-    abort();
+          perror("read");
+          abort();
   }
-
   printf("read %i bytes\n", nread);
+
+  for (ssize_t i = 0; i < nread; ++i) {
+          printf("%02hhx ", (unsigned char)buffer[i]);
+  }
+  printf("\n");*/
+
+  char buffer[1024];
+  struct iovec data;
+  struct msghdr msg;
+  memset(&msg, 0, sizeof msg);
+  data.iov_base = buffer;
+  data.iov_len = sizeof buffer;
+  msg.msg_iov = &data;
+  msg.msg_iovlen = 1;
+
+  ssize_t nread = recvmsg(sockfd, &msg, 0);
+  if (nread < 0) {
+          perror("read");
+          abort();
+  }
+  printf("read %i bytes\n", nread);
+
+  for (ssize_t i = 0; i < nread; ++i) {
+          printf("%02hhx ", (unsigned char)buffer[i]);
+  }
+  printf("\n");
 }
